798dc958c427cdf55ee669426d3c052bc55cda9e
Marc G <mac_gere@yahoo.com>
2022-06-18 13:48:16 -0500

    added styling

--- src/OptionButtons.js
+++ src/OptionButtons.js
@@ -4,12 +4,12 @@ export const OptionButtons = ({unCapitalizeFirst, setNumbers, setPassword, setSp
 
     return (
         <div className='option-buttons'>
-        <button onClick={() => setSpecialChar(randomSpecChar)}>Add/Change Special Character</button>
-        <button onClick={() => setSpecialChar('')}>Remove Special Character</button>
-        <button onClick={() => setNumbers(randomNumber)}>Add/Change Numbers</button>
-        <button onClick={() => setNumbers('')}>Remove Numbers</button>
-        <button onClick={() => setPassword(capitalizeFirst(password))}>Add Capital Letter</button>
-        <button onClick={() => setPassword(unCapitalizeFirst(password))}>Remove Capital Letter</button>
-      </div>
+            <button onClick={() => setSpecialChar(randomSpecChar)}>Add/Change Special Character</button>
+            <button onClick={() => setSpecialChar('')}>Remove Special Character</button>
+            <button onClick={() => setNumbers(randomNumber)}>Add/Change Numbers</button>
+            <button onClick={() => setNumbers('')}>Remove Numbers</button>
+            <button onClick={() => setPassword(capitalizeFirst(password))}>Add Capital Letter</button>
+            <button onClick={() => setPassword(unCapitalizeFirst(password))}>Remove Capital Letter</button>
+        </div>
     )
 }
